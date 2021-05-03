b1cf994afea23ca9bf905ca46cb0b562d881e799
Enric Tomas Grau Luque <etgrau@irec.cat>
2021-05-03 15:11:34 +0200

    Change to functions.py

--- spectrapepper/__init__.py
+++ spectrapepper/__init__.py
@@ -2,8 +2,8 @@
 
 __author__ = """Enric Tomas Grau Luque"""
 __email__ = 'patenric@gmail.com'
-__version__ = '0.0.4'
+__version__ = '0.0.6'
 
 
 """Import all the functions"""
-from spectrapepper import *
+from .functions import *
