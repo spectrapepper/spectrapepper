cec6a349c2064eb884ce40e4ab8d454f4a3cb8fb
Enric Tomas Grau Luque <etgrau@irec.cat>
2021-05-27 20:32:35 +0200

    Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -12,16 +12,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'SpectraPepper'
-copyright = '2021, Enric Tomas Grau Luque'
-author = 'Enric Tomas Grau Luque'
+copyright = '2021, Enric Grau Luque'
+author = 'Enric Grau Luque'
 
 # The short X.Y version
 version = ''
@@ -40,6 +40,7 @@ release = ''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -75,7 +76,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'insegel'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -102,7 +103,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'spectrapepperdoc'
+htmlhelp_basename = 'SpectraPepperdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -129,8 +130,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'spectrapepper.tex', 'spectrapepper Documentation',
-     'Enric Tomas Grau Luque', 'manual'),
+    (master_doc, 'SpectraPepper.tex', 'SpectraPepper Documentation',
+     'Enric Grau Luque', 'manual'),
 ]
 
 
@@ -139,7 +140,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'spectrapepper', 'spectrapepper Documentation',
+    (master_doc, 'spectrapepper', 'SpectraPepper Documentation',
      [author], 1)
 ]
 
@@ -150,8 +151,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'spectrapepper', 'spectrapepper Documentation',
-     author, 'spectrapepper', 'One line description of project.',
+    (master_doc, 'SpectraPepper', 'SpectraPepper Documentation',
+     author, 'SpectraPepper', 'One line description of project.',
      'Miscellaneous'),
 ]
 
