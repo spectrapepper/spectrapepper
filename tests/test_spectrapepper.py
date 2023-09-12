58ad99ae9c1ae83ba9331a8c3ec1f53649b14010
Enric Tomas Grau Luque <etgrau@irec.cat>
2023-09-12 10:31:47 +0200

    test imports

--- tests/test_spectrapepper.py
+++ tests/test_spectrapepper.py
@@ -3,8 +3,8 @@
 """Tests for `spectrapepper` package."""
 
 import unittest
-import functions as spep
-# import spectrapepper as spep
+# import functions as spep
+import spectrapepper as spep
 import numpy as np
 import pandas as pd
 
